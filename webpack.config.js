258131d11a253e363792841c724e22007ab9540a
Trachuk Serhiy <trachukserhiy@gmail.com>
2020-08-02 22:35:52 +0300

    Add webpack plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,39 @@
 const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  context: path.resolve(__dirname, 'src'), // відповідає за те де лажать всі соурси в проекті
-  mode: "development", // режи розробки
-  entry: './index.js', // так як прописано контекст "/src" можна проігнорувати в іншому випадку "./src/index.js"
+  context: path.resolve(__dirname, 'src'),
+  mode: "development",
+  entry: './index.js',
   output: {
-    filename: "bundle.js",
+    filename: "bundle.[hash].js",
     path: path.resolve(__dirname, 'dist')
-  }
-
-
+  },
+  resolve: {
+    extensions: ['.js'],
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      "@core": path.resolve(__dirname, 'src/core')
+    }
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: "index.html"
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src/favicon.ico'),
+          to: path.resolve(__dirname, 'dist')
+        },
+      ],
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'bundle.[hash].css',
+    }),
+  ]
 }
